5f1bf7cd56b97e3dadaa11cb0249d8ec40242c9d
k-ris <christkuzanuka@gmail.com>
2023-08-17 00:41:55 +0100

    Adaptation du design

--- admin/controller/Bootstrap.php
+++ admin/controller/Bootstrap.php
@@ -1,10 +1,11 @@
 <?php
 class Bootstrap{
-    public function menu($titre, $lien = null){
+    public function menu($titre, $lien = null, $icone = "puzzle"){
         return <<<HTML
         <li class="nav-item">
-            <a class="nav-link d-flex align-items-center text-sm active" aria-current="page" href="$lien">
-            $titre
+            <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="$lien">
+                <svg class="bi"><use xlink:href="#{$icone}"/></svg>
+                $titre
             </a>
         </li>
 HTML;
--- admin/layout/groupe_menu.php
+++ admin/layout/groupe_menu.php
@@ -5,21 +5,21 @@
 <div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
     <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
     <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="sidebarMenuLabel">Musuproc UPC</h5>
+        <h5 class="offcanvas-title" id="sidebarMenuLabel">MUSUPROC</h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
     </div>
     
     <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
         <ul class="nav flex-column">
-        <?= $bootstrap->menu("Accueil", 'AdministrateurCtrl.php') ?>
-        <?= $bootstrap->menu("Sensibilisation", '') ?>
-        <?= $bootstrap->menu("Livret de santé", 'LivretCtrl.php') ?>
+        <?= $bootstrap->menu("Accueil", 'AdminstrateurCtrl.php', 'house-fill') ?>
+        <?= $bootstrap->menu("Sensibilisation", 'SensibilisationCtrl.php', 'people') ?>
+        <?= $bootstrap->menu("Livret de santé", 'LivretCtrl.php', 'file-earmark-text') ?>
         </ul>
 
         <hr class="my-3">
 
         <ul class="nav flex-column mb-auto">
-            <?= $bootstrap->menu("Déconnexion", '') ?>
+            <?= $bootstrap->menu("Déconnexion", '', 'door-closed') ?>
         </ul>
     </div>
     </div>
